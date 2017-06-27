95ea1da1fc1e570581f2f03ac376c51ecbca0b69
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-06-27 05:34:47 +0000

    added more tables and models. Work in progress

--- app/Employee.php
+++ app/Employee.php
@@ -10,4 +10,33 @@ class Employee extends Model
 	protected $dates = [
 		'created_at', 'updated_at', 'date_of_birth', 'date_joined'
 	];
+
+	public static function get_by_tarzan_id($tarzan_id = ''){
+		$count = Static::where('tarzan_id', $tarzan_id)->count() > 0;
+		if ($count) {
+			return Static::where('tarzan_id', $tarzan_id)->first();
+		}
+		return 0;
+	}
+
+	public function calculate_total_elements(){
+		return $this->element_car + $this->element_house + $this->element_other;
+	}
+
+	public function calculate_total_emoluments(){
+		return $this->calculate_total_elements() + $this->basic_pay;
+	}
+
+	public function calculate_net_taxable($value='')
+	{
+		# code...
+	}
+
+	public function calculate_tax(){
+
+	}
+
+	public function calculate_take_home(){
+
+	}
 }
---
+++ database/migrations/2017_06_27_043937_create_transactions_table.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('employee_id');
+            $table->unsignedInteger('period_id');
+            $table->string('location');
+            $table->string('designation');
+            $table->decimal('basic_pay',8,2);
+            $table->decimal('element_car',8,2);
+            $table->decimal('element_rent',8,2);
+            $table->decimal('element_other',8,2);
+            $table->boolean('union');
+            $table->boolean('wife');
+            $table->unsignedTinyInteger('children');
+            $table->boolean('contributes_to_ssf');
+            $table->boolean('disabled');
+            $table->boolean('soap');
+            $table->string('mode_of_payment');
+            $table->string('bank_account_number')->nullable();
+            $table->string('bank_name')->nullable();
+            $table->decimal('advance_amount',8,2);
+            $table->unsignedTinyInteger('days_absent');
+            $table->boolean('age_above_55');
+            $table->decimal('ssf_contribution_employee',8,2);
+            $table->decimal('ssf_contribution_employer',8,2);
+            $table->decimal('elements',8,2);
+            $table->decimal('total_emoluments',8,2);
+            $table->decimal('total_ssf_contributions',8,2);
+            $table->decimal('children_marriage_relief',8,2);
+            $table->decimal('disability_relief',8,2);
+            $table->decimal('net_taxable',8,2);
+            $table->decimal('union_dues',8,2);
+            $table->decimal('absence_deduction',8,2);
+            $table->decimal('ff_levy',8,2);
+            $table->decimal('other_additions',8,2);
+            $table->decimal('other_additions_notes',8,2);
+            $table->decimal('other_deductions',8,2);
+            $table->decimal('other_deductions_notes',8,2);
+            $table->decimal('take_home_amount',8,2);
+            $table->decimal('tax_payable',8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
