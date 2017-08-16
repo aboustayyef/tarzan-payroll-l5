ffbc37daa7ad7fa15b5bd602a2aecdcd2f1cdf68
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2017-08-16 09:16:59 +0000

    added Salaries object

---
+++ app/Salaries.php
@@ -0,0 +1,61 @@
+<?php 
+namespace App;
+use App\Period;
+use Illuminate\Database\Eloquent\Collection;
+
+/**
+*   A Salaries object is a collection of Transaction objects
+*/
+
+class Salaries
+{
+    public $transactions;
+    private $locations;
+
+    public function __construct(Collection $transactions)
+    {
+        $this->transactions = $transactions;
+        $this->locations = 
+        [
+            'seniors'   =>  ['Directors', 'Management'],
+            'juniors'   =>  [ "Accra Head Office", "Blue Gallery Permanent", "Container Depot (Tema)", "Tema Admin", "Tema Permanent" ]
+        ];
+    }
+
+    public function all()
+    {
+        return $this->transactions;
+    }
+    
+    public function filterBySeniority($seniority = 'juniors')
+    {
+        if (! in_array($seniority, ['seniors', 'juniors'])) {
+            throw new \Exception("Seniority can be either juniors or seniors");
+        }
+        $locations = $this->locations;
+        return $this->transactions->filter(function($item, $key) use ($locations, $seniority){
+            return in_array($item->location, $locations[$seniority]);
+        });
+    }
+
+    public function filterByLocation($location = 'Tema Admin')
+    {
+        $allLocations = array_merge($this->locations['seniors'], $this->locations['juniors']);
+        if (! in_array($location, $allLocations)) {
+            throw new \Exception("Location does not exist");
+        }
+        return $this->transactions->filter(function($item, $key) use ($location){
+            return $item->location == $location;
+        });
+
+    }
+    public function juniors()
+    {
+       return $this->filterBySeniority('juniors');
+    }
+    public function seniors()
+    {
+       return $this->filterBySeniority('seniors');
+    }
+}
+?>
--- app/Transaction.php
+++ app/Transaction.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
+    private $locations;
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
+    public function scopeSeniors($query)
+    {
+        return $query->whereIn('location', $this->locations['seniors']);
+    }
 
 }
